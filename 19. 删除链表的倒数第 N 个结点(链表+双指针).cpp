576517e90d617adaaa26f8cf2e9cf27fb7183a81
Zhijing Xin <145647884+Xiaojingkuaipao@users.noreply.github.com>
2023-12-22 20:04:08 +0800

    Create 19. 删除链表的倒数第 N 个结点(链表+双指针).cpp

---
+++ 19. 删除链表的倒数第 N 个结点(链表+双指针).cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyhead = new ListNode(0, head);//虚拟头节点
+        ListNode* fast = dummyhead;
+        ListNode* slow = dummyhead;
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+        while (fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* tmp = slow->next;
+        slow->next = tmp->next;
+        delete tmp;
+        return dummyhead->next;
+    }
+};
